cbe7ddda145aaa64543e5302ac5f86eb90b035e8
hzhu <hz@henryzhu.me>
2019-05-11 02:55:08 -0700

    Update Storybook EmojiListbox example

--- stories/EmojiListbox/index.js
+++ stories/EmojiListbox/index.js
@@ -3,11 +3,20 @@ import { Listbox, OptionsList, Option } from "../../src/index.js";
 import { EMOJI_GRID } from "../constants.js";
 
 export default () => (
-  <Listbox focused grid>
+  <Listbox style={{ maxWidth: "530px" }} focused grid>
     {EMOJI_GRID.map((row, i) => (
-      <OptionsList key={i} style={{ display: "flex" }}>
+      <OptionsList key={i} style={{ display: "flex", fontSize: "18px" }}>
         {row.map(cell => (
-          <Option key={cell.name}>{cell.emoji}</Option>
+          <Option
+            key={cell.name}
+            style={{
+              width: "30px",
+              textAlign: "center",
+              paddingLeft: "3px"
+            }}
+          >
+            {cell.emoji}
+          </Option>
         ))}
       </OptionsList>
     ))}
